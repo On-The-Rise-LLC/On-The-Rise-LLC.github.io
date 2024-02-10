aa90b987804f7745a6cd7603091b90ecf9a022ea
Jonas Allen <Webmaster@JonasAllen.Codes>
2024-02-09 19:53:43 -0800

    CONFIG Add Github Pages Config and Action
    Added Github Pages configuration and added actions script for
    rebuild on changes to Github repo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  site: "https://On-The-Rise-LLC.github.io"
 });
